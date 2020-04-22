459721a539ecfc73b50023edf2fa474d25fe51b1
Matt Mckinley <matt.g.mckinley@gmail.com>
2020-04-22 15:16:18 +0100

    7/13 tests pass

--- challenges/week3.js
+++ challenges/week3.js
@@ -7,7 +7,21 @@ function getSquares(nums) {
 
 function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Your code here!
+  // set a variable for the final converted words
+  let result = "";
+  // loop through the array
+  for (let i = 0; i < words.length; i++) {
+    // lowercase every word
+    let currentStr = words[i];
+    let tempStr = currentStr.toLowerCase();
+    // convert first letter to uppercase except first word
+    if (i != 0) {
+      tempStr = tempStr.substr(0, 1).toUpperCase() + tempStr.substr(1);
+    }
+    // add converted to the result variable
+    result = result + tempStr;
+  }
+  return result;
 }
 
 function getTotalSubjects(people) {
@@ -17,22 +31,35 @@ function getTotalSubjects(people) {
   // loop through the object, access the subject property which is an array
   // count how many items are in the array
   // return the total count of all subjects in the array.
-  
+
 }
 
 function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+  // convert the array of objects to an object.
+  let object = {};
+  for (let i = 0; i < menu.length; ++i)
+    Object.assign(object, menu[i]);
+  return object;
+  // compare the ingredients
 }
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  let duplicates = arr1.filter(function(val) {
-    return arr2.indexOf(val) != -1;
-  });
-  return duplicates;
+  
+  //combine the arrays
+  let unique = arr1.concat(arr2)
+
+  // create a new array with only the unique items
+  unique = unique.map((item, i) => unique.includes(item, i + 1) ? item : '')
+
+  // sort the array
+  unique = [...new Set(unique)].filter(n => n).sort();
+  return unique;
+
+
 }
 
 module.exports = {
