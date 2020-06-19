5c78778dc40292c893fab9c03702181a5dd02e9a
Matt Mckinley <matt.g.mckinley@gmail.com>
2020-06-19 12:38:38 +0100

    createRange complete and tested

--- challenges/week10.js
+++ challenges/week10.js
@@ -14,7 +14,9 @@ const sumDigits = n => {
 };
 
 /**
- * This function creates a range of numbers as an array. It received a start, an end and a step. Step is the gap between numbers in the range. For example, if start = 3, end = 11 and step = 2 the resulting range would be: [3, 5, 7, 9, 11]
+ * This function creates a range of numbers as an array. It received a start, an end and a step. 
+ * Step is the gap between numbers in the range. For example, if start = 3, end = 11 and step = 2 
+ * the resulting range would be: [3, 5, 7, 9, 11]
  * Both the start and the end numbers are inclusive.
  * Step is an optional parameter. If it is not provided, assume the step is 1.
  * @param {Number} start
@@ -24,10 +26,18 @@ const sumDigits = n => {
 const createRange = (start, end, step) => {
   if (start === undefined) throw new Error("start is required");
   if (end === undefined) throw new Error("end is required");
+  if (step === undefined) { step = 1; }
+  
+  let rangeArr = [];
+  for (let i = start; i <= end; i += step) {
+    rangeArr.push(i);
+  }
+  return rangeArr;
 };
 
 /**
- * This function takes an array of user objects and their usage in minutes of various applications. The format of the data should be as follows:
+ * This function takes an array of user objects and their usage in minutes of various applications. 
+ * The format of the data should be as follows:
  * [
  *  {
  *    username: "beth_1234",
@@ -52,7 +62,8 @@ const createRange = (start, end, step) => {
  *
  * The function should return an array of usernames of users who have used more than 100 minutes of screentime for a given date.
  * The date will be provided in the format "2019-05-04" (YYYY-MM-DD)
- * For example, if passed the above users and the date "2019-05-04" the function should return ["beth_1234"] as she used over 100 minutes of screentime on that date.
+ * For example, if passed the above users and the date "2019-05-04" the function should return ["beth_1234"] 
+ * as she used over 100 minutes of screentime on that date.
  * @param {Array} users
  */
 const getScreentimeAlertList = (users, date) => {
--- test/week10.test.js
+++ test/week10.test.js
@@ -1,5 +1,6 @@
 const {
     sumDigits,
+    createRange,
     hexToRGB
 } = require("../challenges/week10");
 
@@ -14,7 +15,23 @@ describe("sumDigits", () => {
         expect(sumDigits(123)).toBe(6);
         expect(sumDigits(0)).toBe(0);
     });
-});    
+});   
+
+describe("createRange", () => {
+    test("Throws an error if start not supplied", () => {
+        expect(() => createRange(undefined, 5, 2)).toThrow("start is required");
+    });
+    test("Throws an error if end not supplied", () => {
+        expect(() => createRange(1, undefined, 2)).toThrow("end is required");
+    });
+    test("returns correct array when passed values for start, end and step", () => {
+        expect(createRange(3, 11, 2)).toEqual([3, 5, 7, 9, 11]);
+        expect(createRange(5, 30, 5)).toEqual([5, 10, 15, 20, 25, 30]);
+    });
+    test("returns correct array when passed values for start and end, excluding step", () => {
+        expect(createRange(1, 10)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    });
+});
 
 describe("hexToRGB", () => {
     test("Throws an error if hex string is not passed", () => {
